4e16545312a17db89134a124d3d734005e37a4b7
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-26 13:54:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,14 +2,14 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "M", "black", "moody", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 26, "F", "white", "sassy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "white", "sassy", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "M", "brown", "demanding", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "F", "black", "cheery", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "F", "black", "cheery", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 40, "M", "white", "grumpy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "F", "brown", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "brown", "goofy", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "M", "black", "aggressive", 0);
