047cc8a545511b8e92fbfe57a29f2bb5bae80447
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-26 13:21:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 21, "Male", "Brown", "Fiesty", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL);
