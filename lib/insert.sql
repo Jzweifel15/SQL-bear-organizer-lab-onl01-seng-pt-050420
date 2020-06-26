39aef195e8124de97709c1bc7f59777fa32fc076
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-06-26 13:27:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 21, "Male", "Brown", "Fiesty", 1);
+maleINSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 21, "male", "brown", "fiesty", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "Male", "Black", "Moody", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "male", "black", "moody", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 26, "Female", "White", "Sassy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 26, "female", "white", "sassy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "Male", "Brown", "Demanding", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "male", "brown", "demanding", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "Female", "Black", "Cheery", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "female", "black", "cheery", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 40, "Male", "White", "Grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 40, "male", "white", "grumpy", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "Female", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "female", "brown", "angry", 1);
 
 INSERT INTO bears (name) VALUES (NULL);
